b80d3f8fd46a536f2a98ad2ff295fb903d077390
Miguel Nunez <miguelhero700@gmail.com>
2022-07-04 14:49:16 -0400

    inicializacion de funcion main (prototipo 1)

--- proyecto_final_a.c
+++ proyecto_final_a.c
@@ -1,5 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int menu();
+
+system(cls);
+
+
+
+
+
+
+
+
+
 
 int main () {
+  int init=0;
+  menu;
+  init=menu;
+  switch (init)
+  {
+            case 1:
+            /* code */
+            break;
+            
+            case 2:
+            /* code */
+            break;
+
+            case 3:
+            /* code */
+            break;
+
+            case 4:
+            /* code */
+            break;
+
+            case 5:
+            /* code */
+            break;
+            default:
+            /* code */
+            break;
+  }
   return 0;
 }
+
+
+
