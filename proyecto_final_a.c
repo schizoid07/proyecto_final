a418dfb3a0961199f85ac1aa9bde1996a67627b1
Jenfer Martínez <Jenfermz44@gmil.com>
2022-08-13 10:11:21 -0400

    Añadiendo comentarios y arreglando algunos errores

--- proyecto_final_a.c
+++ proyecto_final_a.c
@@ -1,40 +1,38 @@
-#include <stdio.h>
+#include <stdio.h> 
 #include <stdlib.h>
 #include <string.h>
 
 
-int menu();
-int menu_productos (int user);
+int menu(); //Función correspondiente al menú principal
 
-void continuar();
-int creaStock();
-void imprimeStock();
+int menu_productos (int user); /*Función que ejecuta un sub-menú 
+                                  dedicado al apartado de productos*/
 
-typedef struct datosUsuario
+void continuar();//Función que pausa la ejecución hasta que el usuario indique
+void creaStock();//Función que genera un archivo con el stock de productos
+void imprimeStock();//Función que imprime el stock de productos
+
+typedef struct datosUsuario //struct dedicado a los datos de cada usuario
 {
   char nombre[20], apellido[20], tipoDeCuenta[10];
   int cedula, numeroDeCuenta;
 } d_usuario;
-typedef struct datosProducto
+typedef struct datosProducto //struct dedicado a los datos de cada productos
 {
   char nombre[15];
   int unidades;
   float precio;
 } d_producto;
-typedef struct datosTemporales
-{
-  int itera;
-} temporal;
 
-int main () {
+int main () {//Función principal
   int elect;
   do
   {
-    elect = menu_productos(1);
+    elect = menu_productos(1); //Lamada al sub-menú de productos
   switch (elect)
   {
   case 1:
-    imprimeStock();
+    imprimeStock(); 
     break;
   
   case 2:
@@ -45,17 +43,19 @@ int main () {
     break;
 
   default:
-    printf("Opcion no existente");
+    printf("Opcion no existente"); //Mensaje en caso de ingresar una respuesta no esperada
     break;
   }
-  } while (elect != 3);
+  } while (elect != 3);/*Un bucle que no finalizará hasta 
+                        que el usurio indique, por lo que 
+                        el programa mostrará el menú constantemente*/
   
   return 0;
 }
 
 int menu_productos(int user)
 {
-  system("cls");
+  system("cls"); //Limpiar pantalla
   int election; 
 
   if(user == 1)
@@ -64,49 +64,54 @@ int menu_productos(int user)
     printf("\nDigite según sus necesidades:");
     printf("\n____________________________________________________");
     printf("\n1 - Si desea revisar el stock de productos");
-    printf("\n2 - Si desea retirar productos del stock");
-    printf("\n3 - Si desea añadir productos al stock");
+    printf("\n2 - Si desea añadir productos al stock");
+    printf("\n3 - Si desea retirar productos del stock");
     printf("\n4 - Si desea vaciar el stock");
     printf("\n5 - Si desea salir del sistema\n");
-    scanf("%d", &election);
+    scanf("%d", &election); //Guarda la elección del usuario
   }
   else if (user == 0)
   {
     printf("\nBienvenido al sub-menu de productos para clientes");
-    printf("\nDigite según sus necesidades:");
+    printf("\nDigite segun sus necesidades:");
     printf("\n____________________________________________________");
-    printf("\n1 - Si desea añadir productos al carro de compra");
+    printf("\n1 - Si desea anadir productos al carro de compra");
     printf("\n2 - Si desea retirar producto del carro de compra");
     printf("\n3 - Si desea revisar su carro de compra");
     printf("\n4 - Si desea finalizar su compra");
     printf("\n5 - Si desea cancelar su compra\n");
-    scanf("%d", &election);
+    scanf("%d", &election); //Guarda la elección del usuario
   }
   else
   {
-    printf("Ha ocurrido un error, no se le reconoce en el sistema");
+    printf("Ha ocurrido un error, no se le reconoce en el sistema");/*Mensaje de error en caso 
+                                                                      de que la variable no coincida
+                                                                      por lo cual no se le reconozca 
+                                                                      ni como usuario o admin*/
   }
-  return election;
+  return election;//Devuelve la elección
 }
 
 void continuar()
 {
-  printf("\n\npresione cualquier tecla para continuar");
-  system("pause");
+  printf("\n\npresione cualquier tecla para continuar");/*Un mensaje para indicar a el usuario 
+                                                          qué hacer cuando se pause el programa*/
+  system("pause");//Pausa el programa
 }
-int creaStock()
+void creaStock()
 {
-  system("cls");
+  system("cls");//Limpia pantalla
 
-  int i = 0, n = 0, burbuja;
-  FILE *archivo, *tempoFile;
-  archivo=fopen("Stock.db", "ab");
-  d_producto control;
-  temporal temp;
+  int n = 0; 
+  FILE *archivo; //Puntero para los archivos .db
+  archivo=fopen("Stock.db", "ab"); /*Busca un archivo llamado Stock, debido a los atributos 
+                                    en caso de no existir lo crea y en caso de que sí, todo 
+                                    lo que se escriba se añade al archivo existente en binario*/
+  d_producto control;//Struct para el control del stock
 
   if (archivo == NULL)
   {
-    printf("\nError al crear stock");
+    printf("\nError al crear stock");//Mensaje de error en caso de no poder abrir el archivo
   }
   else
   {
@@ -119,40 +124,25 @@ int creaStock()
         printf("\nDigite el precio del producto: ");
         scanf("%f", &control.precio);
         
-        printf("\n\nDigite segun corresponda: ");
+        printf("\n\nDigite segun corresponda: "); /*Pequeño sub-menu que permite al usuario indicar el número 
+                                                    de veces que va a añadir un producto*/
         printf("\n1- Si desea agregar otro producto ");
         printf("\n2 - Si desea volver al menu anterior \n");
-    
         scanf("%d", &n);
 
-        i++;
-        fwrite(&control, sizeof(d_producto), i, archivo);
+        if (n!= 1 && n!=2)//Condicional en caso de que el usuario digite una respuesta no esperada
+        {
+          printf("\n\nHa ocurrido un error"
+          "\nPor favor digite un el valor correspondiente a las opciones del menu\n\n");
+          continuar();
+        }
+        
+        
+        fwrite(&control, sizeof(d_producto), 1, archivo);//Escribe el input del usuario en Stock
     } while(n!=2);
 
   }
-  
-  fclose(archivo);
-
-
-  tempoFile = fopen("temporal.db", "r+b");
-
-  if (tempoFile == NULL)
-  {
-    printf("Nulll tempoFile");
-  }
-  else
-  {
-  fclose(tempoFile);
-  burbuja+=i;
-  temp.itera=burbuja;
-  tempoFile = fopen("temporal.db", "w+b");
-  fwrite(&temp, sizeof(temporal), 1, tempoFile);    
-  printf("temp.itera: %d", temp.itera);
-  }
-  
-  
-  fclose(tempoFile);
-  return burbuja;
+  fclose(archivo);//Cierra archivo
   continuar();
 }
 void imprimeStock()
@@ -160,43 +150,28 @@ void imprimeStock()
 
     system("cls");
     int j = 0, itera = 0;
-    FILE *archivo, *tempoFile;
-    temporal tempo;
+    FILE *archivo;
 
-    tempoFile = fopen("temporal.db", "rb");
-    if (tempoFile == NULL)
-    {
-      printf("Ha ocurrido un error en la apertura del stock\nCodigo de error: itera");
-    }
-    else
-    {
-      fread(&tempo, sizeof(temporal), 1, tempoFile);
-
-      itera += tempo.itera;
-      printf("itera+= %d ", tempo.itera);
-    }
-    fclose(tempoFile);
-
-    archivo = fopen("Stock.db", "rb");
+    archivo = fopen("Stock.db", "rb");//Abre el archivo Stock uncicamente para lectura de datos en binario
     d_producto imp;
     
     if (archivo == NULL)
     {
-      printf("Ha ocurrido un error en la apertura del stock");
+      printf("Ha ocurrido un error en la apertura del stock");//Mensaje de error en caso de no existir el archivo
     }
     else
     {
-      ferror(archivo);
-      
-        printf("itera: %d", itera);
-        fread(&imp, sizeof(d_producto), itera, archivo);
-
+      fread(&imp, sizeof(d_producto), 1, archivo);//Lee archivo
+        while(!feof(archivo)&&!ferror(archivo))/*Un bucle que no va a terminar hasta que se encuentre 
+                                                con el final del archivo o algún tipo de error*/
+        {
           printf("\nProducto: %s", imp.nombre);
           printf("\nExistencias: %d", imp.unidades);
-          printf("\nPrecio: %f", imp.precio);
-          printf("\n\n_________________________________");
-
-      fclose(archivo);
+          printf("\nPrecio: %.2f", imp.precio);
+          printf("\n_________________________________");
+          fread(&imp, sizeof(d_producto), 1, archivo);
+        }
+      fclose(archivo);//Cierra el archivo
     }
     continuar();
 }
